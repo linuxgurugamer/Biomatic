748873b6055573c00516349cd2b26edcd3a03ede
Jonathan Bayer <linuxgurugamer@gmail.com>
2020-04-27 20:23:26 -0400

    Added BiomaticUI.cs Changes to 1.8.1 csproj for LGG environment

---
+++ Source/BiomaticUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Biomatic
+{
+
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class BiomaticUI
+    {
+
+        public void Awake() // Runs first
+        {
+
+        }
+
+        public void Start()
+        { }
+
+
+        // Following get run multiple times
+
+        public void OnGUI()
+        { }
+
+        public void Update()
+        { }
+
+        public void LateUpdate()
+        { }
+
+        // Runs when being destroyed
+        public void OnDestroy()
+        { }
+    }
+}
